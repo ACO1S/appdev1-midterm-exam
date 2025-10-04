2cf96f07723015e772711cbe04a06e0ca91ec701
Francois Dungca <francoisdungca0521@gmail.com>
2025-10-04 14:15:37 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,16 @@ function App() {
 
   return (
     <>
+  {/* <div id="js-preloader" className="js-preloader">
+    <div className="preloader-inner">
+      <span className="dot" />
+      <div className="dots">
+        <span />
+        <span />
+        <span />
+      </div>
+    </div>
+  </div> */}
 
     <header
     className="header-area header-sticky wow slideInDown"
